eed595d9c8bef47a8ba3d24d6a791a1ff627f154
Matt Spaulding <mrmattspaulding@gmail.com>
2024-04-08 20:35:10 -0400

    adds ip

--- pages/api/ask.js
+++ pages/api/ask.js
@@ -3,34 +3,37 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 import clientPromise from "../../lib/mongodb";
 
+
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("questanda");
-  console.log(req.body)
 
   switch (req.method) {
     case "POST":
+      const body = JSON.parse(req.body)
+      const { title, ip } = body
 
-      let bodyObject = { title: req.body };
+      let bodyObject = { title };
 
       bodyObject.slug = slugify(bodyObject.title)
 
       bodyObject.answer = await generateAnswer(bodyObject.title)
 
       bodyObject.date = new Date(Date.now())
+      bodyObject.ip = ip;
       // await db.collection("questions").createIndex({slug: 1} , {unique:true})
       // await db.collection("questions").createIndex({title: 1 }, {unique:true})
-      debugger
+
       try {
         let myPost = await db.collection("questions").insertOne(bodyObject);
       } catch (e) {
-        debugger
+
         if (e.code == 11000) {
           res.status(200).json({ slug: bodyObject.slug })
           break;
         }
       }
-      debugger
+
       res.status(200).json({ slug: bodyObject.slug })
       break;
   }
@@ -47,7 +50,7 @@ function slugify(str) {
 
 async function generateAnswer(prompt) {
   // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 
   const result = await model.generateContent(prompt);
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,40 @@
 import { useRouter } from "next/router";
 
-export default function Page() {
+export default function Page({ ip }) {
   const router = useRouter();
   async function onSubmit(event) {
     event.preventDefault()
- 
+
     const formData = new FormData(event.target)
     const form_values = Object.fromEntries(formData);
- 
+    const title = form_values.title;
+    const body = { title, ip }
     const response = await fetch('/api/ask', {
       method: 'POST',
-      body: form_values.title,
+      body: JSON.stringify(body),
     })
- 
+
 
     const data = await response.json()
     router.push(data.slug)
 
   }
- 
+
   return (
     <form onSubmit={onSubmit}>
       <input type="text" name="title" />
       <button type="submit">Submit</button>
     </form>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const forwarded = req.headers["x-forwarded-for"]
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+
+  return {
+    props: {
+      ip,
+    },
+  }
 }
